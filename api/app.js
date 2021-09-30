04308104305aee87eaab0e5a56b77bbcb8940198
John de Graaff <john@de-graaff.net>
2021-10-01 00:13:12 +0200

    attempts to install mongodb on rpi:armhf/arm32/armv7, also redis, also docker:mongodb

--- api/app.js
+++ api/app.js
@@ -3,24 +3,80 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/linsysadmin')
-  .then(() => console.log('Connected to MongoDB (db:linsysadmin)...'))
-  .catch(err => console.error('Could not connect to MongoDB...'))
-;
+// const mongoose = require('mongoose');
+// // mongoose.connect('mongodb://localhost/linsysadmin')
+// mongoose.connect('mongodb://localhost:27017/linsysadmin')
+//   .then(() => console.log('Connected to MongoDB (db:linsysadmin)...'))
+//   .catch(err => console.error('Could not connect to MongoDB...'))
+// ;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+let pub_path = path.join(__dirname, 'public');
+console.log("# pub_path = ", pub_path);
 
+var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(pub_path));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+
+// https://en.wikipedia.org/wiki/HTTP_ETag
+// The ETag or entity tag is part of HTTP, the protocol for the World Wide Web. It is one of several mechanisms that HTTP provides for Web cache validation, which allows a client to make conditional requests.
+// app.disable('etag');
+
+// https://lodash.com/
+// Functional programming (FP) guide
+// https://github.com/lodash/lodash/wiki/FP-Guide
+// https://github.com/lodash/lodash
+// npm i --save lodash
+// var _ = require('lodash');
+
+// https://www.bezkoder.com/node-js-watch-folder-changes/
+// npm install chokidar fs-extra
+// const chokidar = require('chokidar');
+// const watcher = chokidar.watch('path/to/folder', { persistent: true });
+// const pub_watcher = chokidar.watch(pub_path, { persistent: true });
+// watcher
+//   .on('add',        path =>   console.log(`File ${path} has been added`))
+//   .on('change',     path =>   console.log(`File ${path} has been changed`))
+//   .on('unlink',     path =>   console.log(`File ${path} has been removed`))
+//   .on('addDir',     path =>   console.log(`Directory ${path} has been added`))
+//   .on('unlinkDir',  path =>   console.log(`Directory ${path} has been removed`))
+//   .on('error',      error =>  console.log(`Watcher error: ${error}`))
+//   .on('ready',      () =>     console.log('Initial scan complete. Ready for changes'))
+// ;
+// pub_watcher
+//   .on('change',     path =>   {
+//     console.log(`############ chokidar: file "${path}" has been changed`);
+//     inc_pub_version();
+//   })
+//   .on('error',      error =>  console.log(`Watcher error: ${error}`))
+//   .on('ready',      () =>     console.log('Initial scan complete. Ready for changes'))
+// ;
+//
+
+// const redis = require('redis');
+// const redisClient = redis.createClient({
+//   host: '127.0.0.1',
+//   port : 6379,
+// });
+// redisClient.on('error', err => {
+//   console.log('# Redis error: ' + err);
+// });
+// redisClient.on('ready',function() {
+//   console.log("# Redis connected..");
+// });
+
+function isObject(val) {
+  if (val === null) { return false;}
+  return ( (typeof val === 'function') || (typeof val === 'object') );
+}
 
 module.exports = app;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,103 @@
 var express = require('express');
-var router = express.Router();
+var userRouter = express.Router();
+// npm i joi --save
+const Joi = require('joi');
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: true},
+  lastname: { type: String, required: true},
+}
+,{ timestamps: true }   // key: createdAt, updatedAt
+);
+const User = mongoose.model('User', userSchema);
+
+userRouter.get('/', async (req, res) => {
+  try {
+    let my_query = req.query;
+    let my_query_last = my_query["last"] || "10";
+    let my_last = parseInt(my_query_last, 10) || 10;
+    // console.log("# my_params", my_params);
+    // console.log("# my_query", my_query);
+    // console.log("# my_last", my_last);
+    let get_users = [];
+    get_users = await User.find().sort({_id:-1}).limit(my_last)
+    res.send(get_users);
+  } catch(err) {
+    console.log("error " + err);
+  }
 });
 
-module.exports = router;
+// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// 2xx success
+// 200 OK
+//   Standard response for successful HTTP requests. The actual response will depend on the request method used. In a GET request, the response will contain an entity corresponding to the requested resource. In a POST request, the response will contain an entity describing or containing the result of the action.[8]
+// 3xx redirection
+// 304 Not Modified (RFC 7232)
+// 4xx client errors
+// 400 Bad Request
+// 401 Unauthorized (RFC 7235)
+// 402 Payment Required
+// 403 Forbidden
+// 404 Not anchorNrFound
+// 405 Method Not Allowed
+// 5xx server errors
+// 500 Internal Server Error
+// 501 Not Implemented
+// 502 Bad Gateway
+// 503 Service Unavailable
+// 504 Gateway Timeout
+
+userRouter.get('/:id', async (req, res) => {
+  const get_user = await User.findById(req.params.id);
+  if (!get_user) return res.status(404).send('The User with the given ID was not found.');
+  res.send(get_user);
+});
+
+userRouter.post('/', async (req, res) => {
+  // https://stackoverflow.com/questions/10849687/express-js-how-to-get-remote-client-address
+  // let remoteAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  const { error } = validateUser(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    let new_user_hash = { 
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    };
+    let new_user = new User(new_user_hash);
+    // console.log("# new_user", new_user);
+    new_user = await new_user.save();
+    res.send(new_user);
+  } catch(err) {
+    console.log("error " + err);
+  }
+});
+
+userRouter.put('/:id', async (req, res) => {
+  const { error } = validateUser(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  const edit_user = await User.findByIdAndUpdate(req.params.id, { firstname: req.body.firstname }, {
+    new: true
+  });
+  if (!edit_user) return res.status(404).send('The User with the given ID was not found.');
+  res.send(edit_user);
+});
+
+userRouter.delete('/:id', async (req, res) => {
+  const del_user = await User.findByIdAndRemove(req.params.id);
+  if (!del_user) return res.status(404).send('The User with the given ID was not found.');
+  res.send(del_user);
+});
+
+function validateUser(User) {
+  const schema = Joi.object({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+  });  
+  const validation = schema.validate(User);
+  return validation;
+}
+
+module.exports = userRouter;
+
+//-EOF
